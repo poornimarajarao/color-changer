a6935246085cc16b1dca54cfb96c4e5f27d70db1
Poornima Rajarao <poornima.r.career@gmail.com>
2025-09-12 21:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('changeColorBtn');
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+button.addEventListener('click', () => {
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+});
